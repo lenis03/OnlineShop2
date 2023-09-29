79e76e12b5225821d1461c50ffd8e77b7654bbb8
lenis <fardintorkamand83@gmail.com>
2023-09-29 15:51:51 +0330

    Add time zone for remove_expired_otps command

--- accounts/management/commands/remove_expired_otps.py
+++ accounts/management/commands/remove_expired_otps.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand
 from datetime import datetime, timedelta
+import pytz
 
 from accounts.models import OtpCode
 
@@ -8,6 +9,6 @@ class Command(BaseCommand):
     help = 'remove all expired opt codes'
 
     def handle(self, *args, **options):
-        expired_time = datetime.now() - timedelta(minutes=2)
+        expired_time = datetime.now(tz=pytz.timezone('Asia/Tehran')) - timedelta(minutes=2)
         OtpCode.objects.filter(created__lt=expired_time).delete()
         self.stdout.write('All expired otp codes removed!')
