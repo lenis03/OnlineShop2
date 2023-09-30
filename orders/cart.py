107a95799a15bca208a604ff418f07b6fb826fa7
lenis <fardintorkamand83@gmail.com>
2023-09-30 19:08:05 +0330

    Create cart module and initialization

---
+++ orders/cart.py
@@ -0,0 +1,14 @@
+
+
+CART_SESSION_ID = 'cart'
+
+
+class Cart:
+    def __init__(self, request):
+        self.session = request.session
+        cart = self.session.get(CART_SESSION_ID)
+
+        if not cart:
+            cart = self.session[CART_SESSION_ID] = {}
+
+        self.cart = cart
