251a0a9a295d2771d697bae7e6cd6f4b7db3bd9f
lenis <fardintorkamand83@gmail.com>
2023-09-30 23:14:51 +0330

    Implement method add for module cart & some change

--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
+
+from products.models import Product
+from orders.forms import AddToCartForm
+from orders.cart import Cart
 
 
 class CartView(View):
@@ -10,5 +14,17 @@ class CartView(View):
 
 
 class CartAddView(View):
-    def get(self, request, product_id):
-        pass
+    form_class = AddToCartForm
+
+    def post(self, request, product_id):
+        cart = Cart(request)
+        product = get_object_or_404(Product, id=product_id)
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            cart.add(product, form.cleaned_data['quantity'])
+
+        return redirect('orders:cart')
+
+
+
