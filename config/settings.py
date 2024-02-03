e18243eb7cae9e7f824caa8e83cec05a4b9064ea
Lenis <fardintorkamand83@gmail.com>
2024-02-03 19:28:07 +0330

    Add MIT license

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+LICENSE = 'LICENSE'
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
